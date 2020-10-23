c97d3ab116b31f7248b3b4db3e12b931789c3f8a
Daniel Cunha <danielcunha54@gmail.com>
2020-10-22 21:52:04 -0300

    feat: transform message neighborhood and announcements

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -4,8 +4,51 @@ export interface Message {
   path: number[]
 }
 
+interface Relation {
+  from: number
+  to: number
+}
+
 export class MessageService {
   process(message: Message): void {
-    console.log(`Received message:`, message)
+    const transformed = {
+      ...message,
+      announcements: this.transformAnnouncements(message),
+      neighborhood: this.transformNeighborhood(message)
+    }
+
+    // TODO: Store values
+    console.log(transformed)
+  }
+
+  private transformNeighborhood({ path = [] }: Message): Relation[] {
+    const neighborhood: Relation[] = []
+
+    for (let index = 1; index < path.length; index++) {
+      neighborhood.push({
+        from: path[index - 1],
+        to: path[index]
+      })
+    }
+
+    return neighborhood
+  }
+
+  private transformAnnouncements({ path = [] }: Message): Relation[] {
+    const announcements: Relation[] = []
+    const reversedPath = [...path].reverse()
+
+    for (let fromIdx = 0; fromIdx < path.length - 1; fromIdx++) {
+      const from = reversedPath[fromIdx]
+
+      for (let toIdx = fromIdx + 1; toIdx < path.length; toIdx++) {
+        announcements.push({
+          from,
+          to: reversedPath[toIdx]
+        })
+      }
+    }
+
+    return announcements
   }
 }
