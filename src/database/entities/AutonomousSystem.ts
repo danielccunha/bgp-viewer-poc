693a8bcfee539cd3df4eaaef045c4bb9d608fed4
Daniel Cunha <danielcunha54@gmail.com>
2020-10-23 20:35:06 -0300

    chore: create AutonomousSystem entity

---
+++ src/database/entities/AutonomousSystem.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export type AutonomousSystem = Document & {
+  number: number
+  peer: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+const AutonomousSystemSchema = new Schema(
+  {
+    number: {
+      type: Number,
+      required: true,
+      unique: true
+    },
+    peer: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default mongoose.model<AutonomousSystem>(
+  'AutonomousSystem',
+  AutonomousSystemSchema
+)
