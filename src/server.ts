d4c6db75bbd81e135e981c4977af23424aa0d802
Daniel Cunha <danielcunha54@gmail.com>
2020-10-26 22:07:47 -0300

    feat: add global error handler

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import cors from 'cors'
 import 'express-async-errors'
 
 import routes from './routes'
+import handleError from './middleware/handleError'
 
 const app = express()
 const port = process.env.PORT || 3333
@@ -16,6 +17,7 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api', routes)
+app.use(handleError)
 
 app.listen(port, () => {
   console.log(`Successfully started server on port \x1b[32m${port}\x1b[0m`)
